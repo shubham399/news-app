5d0354b9b0065499b3535cd9890073b49560d1f4
Shubham Kumar <hello@shubhkumar.in>
2024-01-31 17:07:27 +0530

    Added any

--- src/app/api/news/route.ts
+++ src/app/api/news/route.ts
@@ -33,6 +33,6 @@ export async function GET() {
     const URL = `https://newsapi.org/v2/top-headlines?country=in&apiKey=${process.env.API_KEY}`;;
     const response = await fetch(URL);
     const data = await response.json()
-    let resp = data.articles.filter((a) => a.urlToImage != null)
+    let resp = data.articles.filter((a:any) => a.urlToImage != null)
     return Response.json(resp)
 }
