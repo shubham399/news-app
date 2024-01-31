e5d2821bd7ed7aeb7dc5a7ce1b2239ca6b71c1e2
Shubham Kumar <hello@shubhkumar.in>
2024-02-01 01:08:28 +0530

    Use lang

--- src/app/api/news/route.ts
+++ src/app/api/news/route.ts
@@ -1,7 +1,7 @@
 export async function GET() {
-    const URL = `https://newsapi.org/v2/top-headlines?country=in&apiKey=${process.env.API_KEY}&pageSize=100`;
-    const response = await fetch(URL,{ next: { revalidate: 3600 } });
+    const URL = `https://newsapi.org/v2/top-headlines?language=en&apiKey=${process.env.API_KEY}&pageSize=100`;
+    const response = await fetch(URL, { next: { revalidate: 3600 } });
     const data = await response.json()
-    let resp = data.articles.filter((a:any) => a.urlToImage != null)
+    let resp = data.articles.filter((a: any) => a.urlToImage != null)
     return Response.json(resp)
 }
