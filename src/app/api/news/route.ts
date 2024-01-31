7cc7f7436cb64c62c2939247db960d9068d7f91e
Shubham <hello@shubhkumar.in>
2024-01-31 19:37:21 +0530

    Revalidate every hour

--- src/app/api/news/route.ts
+++ src/app/api/news/route.ts
@@ -1,6 +1,6 @@
 export async function GET() {
     const URL = `https://newsapi.org/v2/top-headlines?country=in&apiKey=${process.env.API_KEY}`;;
-    const response = await fetch(URL);
+    const response = await fetch(URL,{ next: { revalidate: 3600 } });
     const data = await response.json()
     let resp = data.articles.filter((a:any) => a.urlToImage != null)
     return Response.json(resp)
