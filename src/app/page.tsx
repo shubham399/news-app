9f4450fd72308632eb841c349567439fd5f3e532
Shubham Kumar <hello@shubhkumar.in>
2024-02-01 19:31:38 +0530

    Made more fix

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -23,6 +23,8 @@ export default function Home() {
   }, [])
 
   return (
+
+
     <Carousel
       opts={{
         align: "start",
@@ -30,25 +32,51 @@ export default function Home() {
       orientation="vertical"
       className="mt-10 max-w-auto  flex h-dvh flex-col items-center justify-between bg-gray-50"
     >
-      <CarouselContent className="pt-10 p-auto m-auto h-svh w-svh">
-        {Array.from({ length: news.length }).map((_, index) => (
-          <CarouselItem key={index} className="pt-1 pb-2" >
-            <a href={news[index].url}>
-              <NewsCard title={news[index].title} description={news[index].description} image_url={news[index].urlToImage} url={news[index].url} author={news[index].author} />
-            </a>
+      {news.length > 0 ?
+        (<CarouselContent className="pt-10 p-auto m-auto h-svh w-svh">
+          {Array.from({ length: news.length }).map((_, index) => (
+            <CarouselItem key={index} className="pt-1 pb-2" >
+              <a href={news[index].url}>
+                <NewsCard title={news[index].title} description={news[index].description} image_url={news[index].urlToImage} url={news[index].url} author={news[index].author} />
+              </a>
+            </CarouselItem>
+          ))}
+          <CarouselItem key={news.length} className="pt-1 pb-2" >
+            <Card className="overflow-clip cursor-pointer m-auto  p-auto h-[80vh] w-[80vw] lg:h-[80vh] lg:w-[60vw] flex flex-col justify-between">
+              <CardHeader className='flex justify-center items-center'>
+                <CardTitle>No More Content</CardTitle>
+                <CardDescription>Please Try Later</CardDescription>
+              </CardHeader>
+              <CardFooter className="flex justify-between">
+              </CardFooter>
+            </Card>
           </CarouselItem>
-        ))}
-        <CarouselItem key={news.length} className="pt-1 pb-2" >
-          <Card className="overflow-clip cursor-pointer m-auto  p-auto h-[80vh] w-[80vw] lg:h-[80vh] lg:w-[60vw] flex flex-col justify-between">
-            <CardHeader className='flex justify-center items-center'>
-              <CardTitle>No More Content</CardTitle>
-              <CardDescription>Please Try Later</CardDescription>
-            </CardHeader>
-            <CardFooter className="flex justify-between">
-            </CardFooter>
-          </Card>
-        </CarouselItem>
-      </CarouselContent>
+        </CarouselContent>)
+        
+        
+        :
+
+        (<CarouselContent className="pt-10 p-auto m-auto h-svh w-svh">
+          <CarouselItem key={news.length} className="pt-1 pb-2" >
+            <Card className="overflow-clip cursor-pointer m-auto  p-auto h-[80vh] w-[80vw] lg:h-[80vh] lg:w-[60vw] flex flex-col justify-between">
+              <CardHeader className='flex justify-center items-center'>
+                <CardTitle>Please Wait</CardTitle>
+                <CardDescription>
+                  <div role="status">
+                    <svg aria-hidden="true" className="w-8 h-8 text-gray-200 animate-spin dark:text-gray-600 fill-blue-600" viewBox="0 0 100 101" fill="none" xmlns="http://www.w3.org/2000/svg">
+                      <path d="M100 50.5908C100 78.2051 77.6142 100.591 50 100.591C22.3858 100.591 0 78.2051 0 50.5908C0 22.9766 22.3858 0.59082 50 0.59082C77.6142 0.59082 100 22.9766 100 50.5908ZM9.08144 50.5908C9.08144 73.1895 27.4013 91.5094 50 91.5094C72.5987 91.5094 90.9186 73.1895 90.9186 50.5908C90.9186 27.9921 72.5987 9.67226 50 9.67226C27.4013 9.67226 9.08144 27.9921 9.08144 50.5908Z" fill="currentColor" />
+                      <path d="M93.9676 39.0409C96.393 38.4038 97.8624 35.9116 97.0079 33.5539C95.2932 28.8227 92.871 24.3692 89.8167 20.348C85.8452 15.1192 80.8826 10.7238 75.2124 7.41289C69.5422 4.10194 63.2754 1.94025 56.7698 1.05124C51.7666 0.367541 46.6976 0.446843 41.7345 1.27873C39.2613 1.69328 37.813 4.19778 38.4501 6.62326C39.0873 9.04874 41.5694 10.4717 44.0505 10.1071C47.8511 9.54855 51.7191 9.52689 55.5402 10.0491C60.8642 10.7766 65.9928 12.5457 70.6331 15.2552C75.2735 17.9648 79.3347 21.5619 82.5849 25.841C84.9175 28.9121 86.7997 32.2913 88.1811 35.8758C89.083 38.2158 91.5421 39.6781 93.9676 39.0409Z" fill="currentFill" />
+                    </svg>
+                    <span className="sr-only">Loading...</span>
+                  </div>
+
+                </CardDescription>
+              </CardHeader>
+              <CardFooter className="flex justify-between">
+              </CardFooter>
+            </Card>
+          </CarouselItem>
+        </CarouselContent>)}
     </Carousel>
   )
 }
