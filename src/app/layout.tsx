abea93e09ed50748a8333895a9c028fb6d608227
Shubham Kumar <hello@shubhkumar.in>
2024-06-18 15:15:20 +0530

    updated layout

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -17,7 +17,13 @@ export default function RootLayout({
   return (
     <html lang="en">
       <head>
-      <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+        <link rel="manifest" href="/manifest.json" />
+        <meta name="theme-color" content="#000000" />
+        <link rel="apple-touch-icon" href="/vercel.svg" />
+        <meta name="apple-mobile-web-app-capable" content="yes" />
+        <meta name="apple-mobile-web-app-status-bar-style" content="black" />
+        <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no" />
+        <meta name="viewport" content="width=device-width, initial-scale=1.0" />
       </head>
       <body className={inter.className}>{children}</body>
     </html>
